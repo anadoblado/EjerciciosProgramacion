e3c2b1da473a32f0d048f7148a13e80c786471a4
anadoblado <anita_doblado@hotmail.com>
2020-02-01 17:06:54 +0100

    more

---
+++ src/Juegos/arkanoid10Fases1/Ladrillo.java
@@ -0,0 +1,111 @@
+package Juegos.arkanoid10Fases1;
+
+import java.awt.Color;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Ladrillo extends ObjetoAPintar{
+
+	protected  Color color;
+	
+	protected int dureza;
+	public final int ANCHO_LADRILLO = 40;
+	public final int ALTO_LADRILLO = 20;
+
+	
+	public Ladrillo() {
+		super();
+		this.color = color;
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.ancho = ANCHO_LADRILLO;
+		this.alto = ALTO_LADRILLO;
+		this.dureza = dureza;
+	}
+
+	@Override
+	public void paint(Graphics2D g) {
+		g.setColor(getColor());
+		g.fillRect(xCoord, yCoord, ANCHO_LADRILLO, ALTO_LADRILLO);
+		g.setColor(Color.white);
+		g.drawRect(xCoord, yCoord, ANCHO_LADRILLO, ALTO_LADRILLO);
+		g.setColor(Color.black);
+		g.drawRect(xCoord+2, yCoord+(ANCHO_LADRILLO/4), ANCHO_LADRILLO-4, 2);
+		g.drawRect(xCoord+2, yCoord+(ANCHO_LADRILLO/2), ANCHO_LADRILLO-2, 2);
+		
+	}
+	
+	
+	
+
+	/**
+	 * @return the color
+	 */
+	public  Color getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	
+
+	@Override
+	public void seMueve() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected boolean isMarketParaBorrar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void collisionWith(ObjetoAPintar objetoCollisioned) {
+		super.collisionWith(objetoCollisioned);
+		if (objetoCollisioned instanceof Pelota) {
+			dureza --;
+			if (dureza == 0) {
+				this.setMarketParaBorrar(true);
+				this.createExplosion();
+			}
+			
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void createExplosion () {
+		Explosion explota = new Explosion();
+		explota.setxCoord(this.xCoord + 10);
+		explota.setyCoord(this.yCoord + 5);
+		PintaArkanoid.getInstance().addNewObjetoParaLaProximaIteracion(explota);
+	}
+
+	/**
+	 * @return the dureza
+	 */
+	public int getDureza() {
+		return dureza;
+	}
+
+	/**
+	 * @param dureza the dureza to set
+	 */
+	public void setDureza(int dureza) {
+		this.dureza = dureza;
+	}
+	
+
+	
+}
