2d281e9bba728603ddbfa1abab419c2aff725072
anadoblado <anita_doblado@hotmail.com>
2020-03-01 21:58:10 +0100

    coche

---
+++ src/Tema7/GestionConcesionario/modelo/ControladorCoche.java
@@ -0,0 +1,9 @@
+package Tema7.GestionConcesionario.modelo;
+
+public class ControladorCoche extends ControladorBBDD {
+
+	public ControladorCoche() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
